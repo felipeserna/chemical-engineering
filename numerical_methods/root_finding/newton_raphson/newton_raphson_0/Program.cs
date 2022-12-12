004c9c1bf80e151cc0465af2f2c310114673b369
Felipe Serna <felipe.serna@jala-foundation.org>
2022-12-11 19:49:02 -0500

    newton_raphson_0 in C#

---
+++ numerical_methods/root_finding/newton_raphson/newton_raphson_0/Program.cs
@@ -0,0 +1,27 @@
+﻿double x0 = 1.4;
+
+Func<double, double> myFunction = x => Math.Pow(x, 2) - 2;
+Func<double, double> myFunctionPrime = x => 2 * x;
+
+double newtonRaphson = x0 - myFunction(x0) / myFunctionPrime(x0);
+
+Console.WriteLine($"newton_raphson = {newtonRaphson}");
+Console.WriteLine($"Math.Sqrt(2) =   {Math.Sqrt(2)}");
+
+Console.WriteLine("_________________________________________");
+
+double myNewtonRaphson(Func<double, double> f, Func<double, double> df, double x0, double tol)
+{
+    if (Math.Abs(f(x0)) < tol)
+    {
+        return x0;
+    }
+    else
+    {
+        return myNewtonRaphson(f, df, x0 - f(x0) / df(x0), tol);
+    }
+}
+
+double estimate = myNewtonRaphson(myFunction, myFunctionPrime, 1.5, 1e-12);
+Console.WriteLine($"estimate =     {estimate}");
+Console.WriteLine($"Math.Sqrt(2) = {Math.Sqrt(2)}");
